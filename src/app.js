5f7e5ef1bab7169f91d7b556bbb09bc1257a4173
Madhumitha <167957749+madhumithaponnusamy@users.noreply.github.com>
2026-01-20 19:59:35 +0530

    Update database path

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const db = require("./db/db"); 
-
 
 const app = express();
+const db = require("./db/db"); 
+
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-const httpLogger = require("../src/modules/utils/logger");
+const httpLogger = require("./modules/utils/logger");
 
 app.use(httpLogger);
 
@@ -41,7 +41,7 @@ issueModule.setupRoutes(app);
 
 
 // Start server (PORT from env or 5000)
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
