9d1e409a3c035db6c53baaf3600384a92606bf69
madhumithaponnusamy <167957749+madhumithaponnusamy@users.noreply.github.com>
2026-01-22 11:50:43 +0530

    env change

--- db.js
+++ db.js
@@ -1,16 +1,21 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  
 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-console.log("Database is connected")
+
+
+console.log("DB Host:", process.env.MYSQLHOST);
+console.log("DB User:", process.env.MYSQLUSER);
 
 module.exports = db;
